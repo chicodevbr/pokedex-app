ae326014c4a4323d3d66af276d287d9d24308653
chicodevbr <chicodevbr@gmail.com>
2021-11-19 13:33:40 -0300

    navbar add

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,47 +1,23 @@
-import React, { useState } from 'react';
-import Navbar from '@material-tailwind/react/Navbar';
-import NavbarContainer from '@material-tailwind/react/NavbarContainer';
-import NavbarWrapper from '@material-tailwind/react/NavbarWrapper';
-import NavbarBrand from '@material-tailwind/react/NavbarBrand';
-import NavbarToggler from '@material-tailwind/react/NavbarToggler';
-import NavbarCollapse from '@material-tailwind/react/NavbarCollapse';
-import Nav from '@material-tailwind/react/Nav';
-
-import NavLink from '@material-tailwind/react/NavLink';
-import Icon from '@material-tailwind/react/Icon';
-
-export default function NavBar() {
-  const [openMenu, setOpenMenu] = useState(false);
-
+const NavBar = (props) => {
   return (
-    <Navbar color="blue">
-      <NavbarContainer>
-        <NavbarWrapper>
-          <NavbarBrand>POKEDEX</NavbarBrand>
-          <NavbarToggler
-            color="white"
-            onClick={() => setOpenMenu(!openMenu)}
-            ripple="light"
-          />
-        </NavbarWrapper>
-
-        <NavbarCollapse open={openMenu}>
-          <Nav>
-            <NavLink href="/" active="light" ripple="light">
-              <Icon name="language" size="xl" />
-              Discover
-            </NavLink>
-            <NavLink href="login" ripple="light">
-              <Icon name="account_circle" size="xl" />
-              Login
-            </NavLink>
-            <NavLink href="dashboard" ripple="light">
-              <Icon name="settings" size="xl" />
-              Dashboard
-            </NavLink>
-          </Nav>
-        </NavbarCollapse>
-      </NavbarContainer>
-    </Navbar>
+    <nav className="bg-purple-700 relative flex flex-wrap items-center justify-between px-2 py-3 bg-teal-500 mb-3">
+      <div className=" px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <a
+            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+            href="/"
+          >
+            Pokedex
+          </a>
+          <ul className="m-2 flex row-span-1 justify-between text-white">
+            <li className="m-1">Home</li>
+            <li className="m-1">Login</li>
+            <li className="m-1">Dashboard</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
+
+export default NavBar;
